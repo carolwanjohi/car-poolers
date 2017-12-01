094273ac2980038ff79786d14dcd645b6bc00304
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-12-01 15:12:35 +0300

    Create basic landing page Create index view function for the landing page of the site Create index.html as the template for the index view function Create index.css as the stylesheet for the index template Create static folder for static files such as css, js and images Update wsgi.py and settings.py to link with the static folder and its files

--- car/settings.py
+++ car/settings.py
@@ -25,12 +25,14 @@ SECRET_KEY = '9@w(u8^7ddn^w-34od0pr1=ikx8x$g!phoi4ou$qjh&v&=i%yx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'pooler.apps.PoolerConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,5 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
--- pooler/views.py
+++ pooler/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    '''
+    View function for the landing page of the application
+    '''
+    title = 'CarPoolers'
+    message = 'Landing Page'
+    return render(request,'index.html',{"title":title,"message":message})
