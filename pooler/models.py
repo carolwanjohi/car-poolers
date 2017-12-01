4f84da1e3e267b0ccb1b4fefb9b719ad8d46ee73
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-12-01 15:40:48 +0300

    Create Driver model Update project settings to link to Postgres database for the application Create Driver model to define a user who is a driver Create test case for the instance of the Driver class Create test case for the get drivers class method for the Driver class Create migratation to a database schema with the Driver model

--- pooler/models.py
+++ pooler/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model):
+    '''
+    Class that defines a Driver in the application
+    '''
+    first_name = models.CharField(max_length=180)
+
+    last_name = models.CharField(max_length=180)
+
+    phone_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+    @classmethod
+    def get_drivers(cls):
+        '''
+        Function that gets all the drivers in the database
+        '''
+        drivers = Driver.objects.all()
+
+        return drivers
+
+
--- pooler/tests.py
+++ pooler/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Driver
 
 # Create your tests here.
+class DriverTestClass(TestCase):
+    '''
+    Test case for the Driver class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Driver class
+        '''
+        # Create instance of D class
+        self.new_driver = Driver(first_name="James",last_name="Muriuki",phone_number=0712345656)
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_driver in an instance of Driver class
+        '''
+        self.assertTrue( isinstance(self.new_driver, Driver) )
+
+    def test_get_drivers(self):
+        '''
+        Test case to check if all drivers are gotten from the database
+        '''
+        gotten_drivers = Driver.get_drivers()
+
+        drivers = Driver.objects.all()
+
+        self.assertTrue( len(gotten_drivers) == len(drivers))
